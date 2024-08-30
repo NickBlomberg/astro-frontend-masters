ac5dacbebadd13a8f722e732380fd65917b2e1fd
Nick Blomberg <nick.blomberg@gmail.com>
2024-08-30 14:40:36 +0100

    feat: site ready to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,20 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import solid from '@astrojs/solid-js';
+
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://astro-frontend-masters.netlify.app',
+	output: 'hybrid',
+	integrations: [
+		solid({
+			include: ['/src/components/cart.tsx'],
+		}),
+		react({
+			include: ['/src/components/add-to-cart.tsx'],
+		}),
+	],
+	adapter: netlify(),
+});
